9f242e069382805ff58d1aa4e539ed35a8758255
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-17 14:53:57 -0500

    new xml request added

--- front-end/main.js
+++ front-end/main.js
@@ -40,5 +40,14 @@ const postUser = (parameters) => {
   })
 }
 
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('loadend', (event) => {
+  console.log(event.target.response);
+})
+
+xhr.open('GET', 'http://localhost:3000/users');
+xhr.send();
+
 
 module.exports = postUser;
