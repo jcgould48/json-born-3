ed935850cb2dea6ce393ebaef5180ad785a30a77
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-17 15:35:02 -0500

    we added something

--- front-end/main.js
+++ front-end/main.js
@@ -2,50 +2,69 @@
 const xhr = new XMLHttpRequest();
 
 
-const getUser = (i) => {
-  xhr.readFile('./users.json', (error, data) => {
-    if (error) {
-      throw error;
-    }
-    const users = JSON.parse(data);
-    console.log(users.find((user) => user.index === i));
-  });
-}
+// const getUser = (i) => {
+//   xhr.readFile('./users.json', (error, data) => {
+//     if (error) {
+//       throw error;
+//     }
+//     const users = JSON.parse(data);
+//     console.log(users.find((user) => user.index === i));
+//   });
+// }
 
-module.exports = getUser;
+// // module.exports = getUser;
 
 
-const postUser = (parameters) => {
-  const [name, age, eyeColor] = parameters;
-  xhr.readFile('./users.json', (error, data) => {
-    if (error) {
-      throw error;
-    }
+// const postUser = (parameters) => {
+//   const [name, age, eyeColor] = parameters;
+//   xhr.readFile('./users.json', (error, data) => {
+//     if (error) {
+//       throw error;
+//     }
     
-    const users = JSON.parse(data);
-    const newUser = {
-      name,
-      age,
-      eyeColor,
-      index: users.length,
-    }
-    users.push(newUser);
-    const updatedUsers = JSON.stringify(users, null, 2);
-    xhr.writeFile('./users.json', updatedUsers, (error) => {
-      if (error) {
-        throw error;
-      }
-    });
-  })
-}
+//     const users = JSON.parse(data);
+//     const newUser = {
+//       name,
+//       age,
+//       eyeColor,
+//       index: users.length,
+//     }
+//     users.push(newUser);
+//     const updatedUsers = JSON.stringify(users, null, 2);
+//     xhr.writeFile('./users.json', updatedUsers, (error) => {
+//       if (error) {
+//         throw error;
+//       }
+//     });
+//   })
+// }
 
 
 xhr.addEventListener('loadend', (id) => {
   console.log(event.target.response);
 })
 
-xhr.open('GET', `http://localhost:3000/users[${id}]`);
+xhr.open('GET', `http://localhost:3000/users`);
 xhr.send();
 
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', "http://localhost:3000/users", true);
+// xhr.responseType = 'json';
 
-module.exports = postUser;
+// xhr.onload = function() {
+
+//   var status = xhr.status;
+  
+//   if (status == 200) {
+//       callback(null, xhr.response);
+//   } else {
+//       callback(status);
+//   }
+// };
+
+// xhr.send();
+
+
+
+
+// module.exports = postUser;
